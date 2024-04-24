6e247fa0c8d3a9764fc41bdedc8c707ff743d89b
ritu-477 <ritujoharbishnoi@gmail.com>
2024-04-24 10:23:16 +0530

    scroll to top-button done

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,3 +4,20 @@ function openNav() {
     document.getElementById("navbar").classList.toggle("start-0");
     document.body.classList.toggle("overflow-hidden")
 }
+
+// scroll-to -top-button////
+const mybutton = document.querySelector(".topBtn");
+const show = document.querySelector(".show")
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        mybutton.classList.add("show")
+    }
+    else {
+        mybutton.classList.remove("show")
+    }
+})
+mybutton.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+
+});
